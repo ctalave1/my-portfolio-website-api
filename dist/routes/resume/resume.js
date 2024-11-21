0efc9e4f3169129392cf5d4361c989f5284d4659
ctalave1 <talavera.christopher@yahoo.com>
2024-11-21 15:44:42 -0500

    Attempting a different way of adding resume pdf  to dist dir in Vercel deployment

--- dist/routes/resume/resume.js
+++ dist/routes/resume/resume.js
@@ -5,7 +5,7 @@ const fs_1 = require("fs");
 require("../../config");
 const router = (0, express_1.Router)();
 router.get('/', (req, res) => {
-    let stream = (0, fs_1.createReadStream)(`${process.env.NODE_ENV === 'development' ? 'public' : 'server/public'}/pdfs/resume.pdf`);
+    let stream = (0, fs_1.createReadStream)(`${process.env.NODE_ENV === 'development' ? 'public' : 'dist/assets'}/pdfs/resume.pdf`);
     let filename = encodeURIComponent('resume.pdf');
     res.setHeader('Content-disposition', `inline; filename="${filename}"`);
     res.setHeader('Content-type', 'application/pdf');
--- src/routes/resume/resume.ts
+++ src/routes/resume/resume.ts
@@ -5,7 +5,7 @@ import '../../config';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  let stream = createReadStream(`public/pdfs/resume.pdf`);
+  let stream = createReadStream(`${process.env.NODE_ENV === 'development' ? 'public' : 'dist/assets'}/pdfs/resume.pdf`);
   let filename = encodeURIComponent('resume.pdf');
 
   res.setHeader('Content-disposition', `inline; filename="${filename}"`);

62cac41d4702b14b39f3d90d35a838272e1178f1
Christopher Talavera <talavera.christopher@yahoo.com>
2024-11-26 22:21:17 -0500

    Remove unnecessary information from API splash page and logs
    - Removing unnecessary information from API splash page and logs about what files are in the current working directory being used to figure out how to serve up my static resume pdf file

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
-const fs_1 = __importDefault(require("fs"));
 const app_1 = __importDefault(require("./app"));
 const server = http_1.default.createServer(app_1.default);
 const port = process.env.PORT || 5000;
 app_1.default.get('/', (_req, res) => {
-    res.send(`my-portfolio-website-api is running on port ${port} | env: ${process.env.NODE_ENV} | frontend_url: ${process.env.FE_URL} | files: ${fs_1.default.readdirSync(`${process.cwd()}`)}}`);
+    res.send(`my-portfolio-website-api is running on port ${port} | env: ${process.env.NODE_ENV}`);
 });
 app_1.default.get('/ping', (_req, res) => {
     res.send('pong');
 });
-server.listen(port, () => console.log(`my-portfolio-website-api is running on port ${port} | env: ${process.env.NODE_ENV} | frontend_url: ${process.env.FE_URL}`));
+server.listen(port, () => console.log(`my-portfolio-website-api is running on port ${port} | env: ${process.env.NODE_ENV}`));
